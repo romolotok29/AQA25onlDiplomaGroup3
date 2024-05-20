706825f65989de0345bfc991bbeda3469eccb78e
rmolotok <roland.from.laptop@gmail.com>
2024-05-20 15:31:42 +0300

    Added new BasePage class based on WaitsService, added second constructor

---
+++ src/test/java/baseEntities/BasePage.java
@@ -0,0 +1,37 @@
+package baseEntities;
+
+import configuration.ReadProperties;
+import core.WaitsService;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public abstract class BasePage {
+
+    protected WebDriver driver;
+    protected WaitsService wait;
+
+    public BasePage(WebDriver driver) {
+        this(driver, false);
+    }
+
+    public BasePage(WebDriver driver, boolean openPageByUrl) {
+        this.driver = driver;
+        this.wait = new WaitsService(driver);
+
+        if (openPageByUrl) {
+            openPageByUrl();
+        }
+    }
+
+    protected abstract By getPageIdentifier();
+    protected abstract String getPagePath();
+
+
+    public Boolean isPageOpened() {
+        return wait.waitForVisibility(getPageIdentifier()).isDisplayed();
+    }
+
+    public String openPageByUrl(){
+        return ReadProperties.getUrl() + getPagePath();
+    }
+}
